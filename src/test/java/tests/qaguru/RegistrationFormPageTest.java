e46784f42595c3c390c1322e40c8ba9017670776
Maksim Golyanitskiy <golyanitskiy@gmail.com>
2022-05-15 22:37:12 +0400

    added tag

--- src/test/java/tests/qaguru/RegistrationFormPageTest.java
+++ src/test/java/tests/qaguru/RegistrationFormPageTest.java
@@ -1,6 +1,7 @@
 package tests.qaguru;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationFormPage;
 
@@ -8,6 +9,7 @@ import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormPageTest extends TestBase{
     @Test
+    @Tag("general")
     @DisplayName("Successful form filling test")
     void formFilling() {
         RegistrationFormPage page = new RegistrationFormPage();
